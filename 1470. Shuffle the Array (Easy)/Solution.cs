e952dda91d6c5748d2b8b0b4f9df5712d8b9e2d8
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-06 22:18:28 +0200

    Solved 69, 1470

---
+++ 1470. Shuffle the Array (Easy)/Solution.cs
@@ -0,0 +1,13 @@
+﻿public class Solution
+{
+    public int[] Shuffle(int[] nums, int n)
+    {
+        int[] result = new int[n*2];
+        for (int i = 0; i < nums.Length; i+=2)
+        {
+            result[i] = nums[i/2];
+            result[i+1] = nums[n++];
+        }
+        return result;
+    }
+}
