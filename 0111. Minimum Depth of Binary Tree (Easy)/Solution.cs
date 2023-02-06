eaae3e17d2bbd46d6c45f7a9310280d93ad8e581
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-06 22:33:45 +0200

    Solved 111

---
+++ 0111. Minimum Depth of Binary Tree (Easy)/Solution.cs
@@ -0,0 +1,11 @@
+﻿public class Solution
+{
+    public int MinDepth(TreeNode root)
+    {
+        if (root == null) return 0;
+        int left = MinDepth(root.left);
+        int right = MinDepth(root.right);
+        if (left == 0 || right == 0) return 1 + Math.Max(left, right);
+        return 1 + Math.Min(left, right);
+    }
+}
