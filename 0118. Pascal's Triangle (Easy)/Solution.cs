d651b61e9ab937c98605f5712ba71b0dea506681
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-07 19:16:38 +0200

    Solved 118

---
+++ 0118. Pascal's Triangle (Easy)/Solution.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public IList<IList<int>> Generate(int numRows)
+    {
+        IList<IList<int>> rows = new List<IList<int>>() { new List<int>() { 1 } };
+        for (int i = 1; i < numRows; i++)
+        {
+            IList<int> row = new List<int>();
+            row.Add(1);
+            for (int j = 1; j < i; j++) row.Add(rows[i - 1][j] + rows[i - 1][j - 1]);
+            row.Add(1);
+            rows.Add(row);
+        }
+        return rows;
+    }
+}
