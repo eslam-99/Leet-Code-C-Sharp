f546a2050b331174b8cced4ad07b7a51c4ecf9d5
eslam-99 <eslam.mohamed.551999@gmail.com>
2023-02-07 18:29:11 +0200

    Delete 0904. Fruit Into Baskets (Easy) directory

--- 0904. Fruit Into Baskets (Easy)/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Solution s = new Solution();
-        Console.WriteLine(s.TotalFruit(new[] { 0, 1, 2, 2 }));
-    }
-}
