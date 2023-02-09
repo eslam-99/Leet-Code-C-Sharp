f2658c7a9ab4ceede2e9de8ac06511dd6a4e5990
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-09 23:43:57 +0200

    Solved 141

---
+++ 0141. Linked List Cycle (Easy)/Solution.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public bool HasCycle(ListNode head)
+    {
+        ListNode oneStep = head, twoSteps = head;
+        while (twoSteps is not null && twoSteps.next is not null)
+        {
+            oneStep= oneStep.next;
+            twoSteps = twoSteps.next.next;
+            if (oneStep == twoSteps) return true;
+        }
+        return false;
+    }
+}
