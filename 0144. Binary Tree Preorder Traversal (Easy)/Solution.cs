8ff53778aa0c5caa8b159e6a69e7f65934933cd0
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-09 23:48:40 +0200

    Solved 144

---
+++ 0144. Binary Tree Preorder Traversal (Easy)/Solution.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public IList<int> PreorderTraversal(TreeNode root)
+    {
+        if (root is null) return new List<int>();
+        List<int> result = new List<int>();
+        result.Add(root.val);
+        result.AddRange(PreorderTraversal(root.left));
+        result.AddRange(PreorderTraversal(root.right));
+        return result;
+    }
+}
