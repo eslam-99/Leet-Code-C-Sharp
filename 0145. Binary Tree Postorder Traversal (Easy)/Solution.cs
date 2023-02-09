c610a16d0393d90e8a656368022b8d49c5ee66c9
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-09 23:50:55 +0200

    Solved 145

---
+++ 0145. Binary Tree Postorder Traversal (Easy)/Solution.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public IList<int> PostorderTraversal(TreeNode root)
+    {
+        if (root is null) return new List<int>();
+        List<int> result = new List<int>();
+        result.AddRange(PostorderTraversal(root.left));
+        result.AddRange(PostorderTraversal(root.right));
+        result.Add(root.val);
+        return result;
+    }
+}
