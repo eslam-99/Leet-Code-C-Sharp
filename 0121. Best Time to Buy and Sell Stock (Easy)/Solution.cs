da723de738c6cd3c1060fa8ae670c71f278beebe
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-09 16:26:33 +0200

    Solved 121

---
+++ 0121. Best Time to Buy and Sell Stock (Easy)/Solution.cs
@@ -0,0 +1,13 @@
+﻿public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int minValIndex = 0, maxProfit = 0;
+        for (int i = 1; i < prices.Length; i++)
+            if (prices[i] < prices[i - 1] && prices[i] < prices[minValIndex])
+                minValIndex = i;
+            else if (prices[i] - prices[minValIndex] > maxProfit)
+                maxProfit = prices[i] - prices[minValIndex];
+        return maxProfit;
+    }
+}
