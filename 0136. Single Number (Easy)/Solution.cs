bbd61337043c384d4254aa6f76069ba77bc0f535
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-09 23:01:31 +0200

    Solved 136

---
+++ 0136. Single Number (Easy)/Solution.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        return SingleNumber_V2(nums);
+    }
+    public int SingleNumber_V1(int[] nums)
+    {
+        HashSet<int> visited = new();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (visited.Contains(nums[i])) visited.Remove(nums[i]);
+            else visited.Add(nums[i]);
+        }
+        return visited.First();
+    }
+    public int SingleNumber_V2(int[] nums)
+    {
+        int mask = 0;
+        foreach (int num in nums) mask ^= num;
+        return mask;
+    }
+}
