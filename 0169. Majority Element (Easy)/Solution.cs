fc092a44bbb2ce216cb4b091541c7ee645d19cbe
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-10 22:43:53 +0200

    Solved 169

---
+++ 0169. Majority Element (Easy)/Solution.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        int candidate = 0, counter = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (counter == 0) candidate = nums[i];
+            if (nums[i] == candidate) counter++;
+            else counter--;
+        }
+        return candidate;
+    }
+}
