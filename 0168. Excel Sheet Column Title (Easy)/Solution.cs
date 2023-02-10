5a5fdb31ef212949659d2be0bac9c4adc697439a
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-10 20:43:16 +0200

    Solved 168

---
+++ 0168. Excel Sheet Column Title (Easy)/Solution.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public string ConvertToTitle(int columnNumber)
+    {
+        string result = "";
+        while (columnNumber > 0)
+        {
+            int mod = (columnNumber - 1) % 26;
+            result = (char)(mod + 65) + result;
+            columnNumber = (columnNumber - mod) / 26;
+        }
+        return result;
+    }
+}
