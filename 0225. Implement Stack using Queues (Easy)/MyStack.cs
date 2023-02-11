0ead913aa58a65b0bf692f1f2275f66a0f127006
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-11 18:57:43 +0200

    Solved 225

---
+++ 0225. Implement Stack using Queues (Easy)/MyStack.cs
@@ -0,0 +1,31 @@
+﻿public class MyStack
+{
+    Queue<int> queue;
+
+    public MyStack()
+    {
+        queue = new Queue<int>();
+    }
+
+    public void Push(int x)
+    {
+        int c = queue.Count;
+        queue.Enqueue(x);
+        while (c-- > 0) queue.Enqueue(queue.Dequeue());
+    }
+
+    public int Pop()
+    {
+        return queue.Dequeue();
+    }
+
+    public int Top()
+    {
+        return queue.Peek();
+    }
+
+    public bool Empty()
+    {
+        return queue.Count == 0;
+    }
+}
---
+++ 0225. Implement Stack using Queues (Easy)/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MyStack obj = new MyStack();
+        obj.Push(4);
+        int param_2 = obj.Pop();
+        int param_3 = obj.Top();
+        bool param_4 = obj.Empty();
+    }
+}
