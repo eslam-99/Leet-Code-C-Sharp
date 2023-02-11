42921deedb329e84302a5db216b2a41648180481
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-11 13:36:39 +0200

    Solved 171

---
+++ 0171. Excel Sheet Column Number (Easy)/Solution.cs
@@ -0,0 +1,10 @@
+﻿public class Solution
+{
+    public int TitleToNumber(string columnTitle)
+    {
+        int result = ((int)columnTitle[0] - 64);
+        for (int i = 1; i < columnTitle.Length; i++)
+            result = result * 26 + ((int)columnTitle[i] - 64);
+        return result;
+    }
+}
