43fa94311a45e5b255883f75d6edecbdb2b76b3f
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-11 16:31:54 +0200

    Solved 205

---
+++ 0205. Isomorphic Strings (Easy)/Solution.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        Dictionary<char, char> to = new(26);
+        Dictionary<char, char> from = new(26);
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (to.ContainsKey(s[i]))
+            {
+                if (to[s[i]] != t[i]) return false;
+            }
+            else to.Add(s[i], t[i]);
+            if (from.ContainsKey(t[i]))
+            {
+                if (from[t[i]] != s[i]) return false;
+            }
+            else from.Add(t[i], s[i]);
+        }
+        return true;
+    }
+}
