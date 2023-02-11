ef0888af29440bc4b8891d44f1b40b873247b15f
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-11 15:38:55 +0200

    Solved 203

---
+++ 0203. Remove Linked List Elements (Easy)/Solution.cs
@@ -0,0 +1,13 @@
+﻿public class Solution
+{
+    public ListNode RemoveElements(ListNode head, int val)
+    {
+        while (head is not null && head.val == val) head = head.next;
+        if (head is null) return null!;
+        ListNode h1 = head;
+        while (h1.next is not null)
+            if (h1.next.val == val) h1.next = h1.next.next;
+            else h1 = h1.next;
+        return head;
+    }
+}
