bd6d506fcce56eb92b3836f919c3469f70aa2121
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-11 18:38:48 +0200

    Solved 219

---
+++ 0219. Contains Duplicate II (Easy)/Solution.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        if (k == 0) return false;
+        Dictionary<int, int> d = new();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (d.ContainsKey(nums[i]))
+            {
+                if (i - d[nums[i]] <= k) return true;
+                d[nums[i]] = i;
+            }
+            else d.Add(nums[i], i); 
+        }
+        return false;
+    }
+}
