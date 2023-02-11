9c4ced4836aa62bc5a4ab0bcdf61739077b1b976
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-11 17:57:56 +0200

    Solved 206

---
+++ 0206. Reverse Linked List (Easy)/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution s = new();
+        ListNode h = new ListNode(1, new(2, new(3, new(4, new(5)))));
+        h = s.ReverseList(h);
+        while (h is not null)
+        {
+            Console.Write($"{h.val} -> ");
+            h = h.next;
+        }
+        Console.WriteLine("NULL");
+    }
+}
---
+++ 0206. Reverse Linked List (Easy)/Solution.cs
@@ -0,0 +1,10 @@
+﻿public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        if (head is null || head.next is null) return head!;
+        ListNode next = null!;
+        while (head is not null) (head, next, head.next) = (head.next, head, next);
+        return next;
+    }
+}
