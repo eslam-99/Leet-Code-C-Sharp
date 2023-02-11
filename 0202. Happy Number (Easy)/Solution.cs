afd6626c0db6c2ece26ccb721a2cacd8a0b850f7
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-11 15:13:36 +0200

    Solved 202

---
+++ 0202. Happy Number (Easy)/Solution.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public bool IsHappy(int n)
+    {
+        while (n != 1 && n != 4) n = GetNext(n);
+        return n == 1;
+    }
+    private int GetNext(int n)
+    {
+        int sum = 0;
+        while (n > 0)
+        {
+            int d = n % 10;
+            n /= 10;
+            sum += d * d;
+        }
+        return sum;
+    }
+}
