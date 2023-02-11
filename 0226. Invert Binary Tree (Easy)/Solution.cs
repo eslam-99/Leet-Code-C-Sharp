301f4e25775d4de1caf2a3b2933c852faad78e58
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-11 19:09:08 +0200

    Solved 226

---
+++ 0226. Invert Binary Tree (Easy)/Solution.cs
@@ -0,0 +1,9 @@
+﻿public class Solution
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root is null) return null!;
+        (root.left, root.right) = (InvertTree(root.right), InvertTree(root.left));
+        return root;
+    }
+}
