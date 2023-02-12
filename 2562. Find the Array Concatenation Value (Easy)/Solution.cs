376fa674fbef76bb21eb6cf8690d5238985a6492
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-12 18:28:12 +0200

    Solved 2562

---
+++ 2562. Find the Array Concatenation Value (Easy)/Solution.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public long FindTheArrayConcVal(int[] nums)
+    {
+        long sum = 0;
+        int start = 0, end = nums.Length - 1;
+        while (start < end)
+            sum += int.Parse(nums[start++].ToString() + nums[end--].ToString());
+        if (start == end) sum += nums[start];
+        return sum;
+    }
+}
