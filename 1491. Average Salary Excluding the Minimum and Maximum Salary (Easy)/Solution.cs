f7c36773555a0007aca094557e4d1aabd8b05a16
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-12 18:51:10 +0200

    Solved 1491

---
+++ 1491. Average Salary Excluding the Minimum and Maximum Salary (Easy)/Solution.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public double Average(int[] salary)
+    {
+        int min = salary[0], max = salary[0], sum = salary[0];
+        for (int i = 1; i < salary.Length; i++)
+        {
+            min = Math.Min(min, salary[i]);
+            max = Math.Max(max, salary[i]);
+            sum += salary[i];
+        }
+        return (double)(sum - min - max) / (salary.Length - 2);
+    }
+}
