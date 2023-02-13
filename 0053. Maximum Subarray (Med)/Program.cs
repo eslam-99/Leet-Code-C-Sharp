ce4095977729530a57d073f97a1c59976d6abddf
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-13 20:52:23 +0200

    Solved 53

---
+++ 0053. Maximum Subarray (Med)/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution s = new();
+        Console.WriteLine(s.MaxSubArray(new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+        Console.WriteLine(s.MaxSubArray(new[] { 1 }));
+        Console.WriteLine(s.MaxSubArray(new[] { 5, 4, -1, 7, 8 }));
+        /*
+            SUM = 1
+            SUM = 3
+            SUM = 2
+            SUM = 5
+            SUM = 1
+            SUM = 6 ==> MAX
+            SUM = 5
+            SUM = 3
+            SUM = 4
+        */
+    }
+}
---
+++ 0053. Maximum Subarray (Med)/Solution.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int MaxSubArray(int[] nums)
+    {
+        int sum = 0, max = int.MinValue;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            max = Math.Max(max, sum);
+            sum = Math.Max(0, sum);
+        }
+        return max;
+    }
+}
