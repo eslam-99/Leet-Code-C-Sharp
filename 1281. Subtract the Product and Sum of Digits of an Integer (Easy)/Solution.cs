7db04e58f88210a2f7502498cec199cdc04630b6
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-13 15:06:28 +0200

    Solved 1281

---
+++ 1281. Subtract the Product and Sum of Digits of an Integer (Easy)/Solution.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int SubtractProductAndSum(int n)
+    {
+        int sum = 0, prod = 1;
+        while (n > 0)
+        {
+            int x = n % 10;
+            sum += x;
+            prod *= x;
+            n /= 10;
+        }
+        return prod - sum;
+    }
+}
