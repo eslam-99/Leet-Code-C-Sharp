b6b44329794a97c329a067a3e7f74beecd605669
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-13 21:25:23 +0200

    Solved 2477

---
+++ 2477. Minimum Fuel Cost to Report to the Capital (Med)/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution s = new();
+        long res = s.MinimumFuelCost(new int[][]{
+            new int[] { 0, 1 },
+            new int[] { 0, 2 },
+            new int[] { 0, 3 } },
+        5);
+        Console.WriteLine(res + " == 3");
+        res = s.MinimumFuelCost(new int[][]{ 
+            new int[] { 3 , 1 },
+            new int[] { 3 , 2 },
+            new int[] { 1 , 0 },
+            new int[] { 0 , 4 },
+            new int[] { 0 , 5 },
+            new int[] { 4 , 6 }
+        }, 2);
+        Console.WriteLine(res + " == 7");
+    }
+}
---
+++ 2477. Minimum Fuel Cost to Report to the Capital (Med)/Solution.cs
@@ -0,0 +1,38 @@
+﻿public class Solution
+{
+    private long minFuel;
+    private long seats;
+    private Dictionary<int, List<int>> adj;
+    public long MinimumFuelCost(int[][] roads, int seats)
+    {
+        if (roads.Length == 0) return 0;
+        this.minFuel = 0;
+        this.seats = seats;
+        this.adj = new();
+
+        foreach (var road in roads)
+        {
+            if (!adj.ContainsKey(road[0])) adj.Add(road[0], new());
+            if (!adj.ContainsKey(road[1])) adj.Add(road[1], new());
+            adj[road[0]].Add(road[1]);
+            adj[road[1]].Add(road[0]);
+        }
+
+        CheckPath(-1, 0);
+
+        return minFuel;
+    }
+
+    private int CheckPath(int parent, int node)
+    {
+        int representatives = 1;
+        foreach (var child in adj[node])
+        {
+            if (child == parent) continue;
+            representatives += CheckPath(node, child);
+        }
+
+        if (node != 0) minFuel += (long) Math.Ceiling((double)representatives / seats);
+        return representatives;
+    }
+}
