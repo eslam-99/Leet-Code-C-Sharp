68df39de9064d1f414ec1cef748ad966d4b3ee78
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-14 16:40:09 +0200

    Solved 1779

---
+++ 1779. Find Nearest Point That Has the Same X or Y Coordinate (Easy)/Solution.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int NearestValidPoint(int x, int y, int[][] points)
+    {
+        int minIndex = -1;
+        for (int i = 0; i < points.Length; i++)
+        {
+            if (points[i][0] == x || points[i][1] == y)
+            {
+                minIndex = i;
+                break;
+            }
+        }
+        if (minIndex == -1) return minIndex;
+
+        for (int i = minIndex + 1; i < points.Length; i++)
+        {
+            if (points[i][0] == x || points[i][1] == y)
+            {
+                if (Math.Abs(x - points[i][0]) + Math.Abs(y - points[i][1]) < 
+                    Math.Abs(x - points[minIndex][0]) + Math.Abs(y - points[minIndex][1]))
+                    minIndex = i;
+            }
+        }
+
+        return minIndex;
+    }
+}
