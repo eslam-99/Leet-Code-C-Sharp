f4ede405d11bcb8e3367f0c81c86c6b81a635f74
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-14 16:24:14 +0200

    Solved 976

---
+++ 0976. Largest Perimeter Triangle (Easy)/Solution.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public int LargestPerimeter(int[] nums)
+    {
+        Array.Sort(nums);
+        for (int i = nums.Length - 1; i > 1; i--)
+        {
+            if (nums[i - 2] + nums[i - 1] > nums[i]) return nums[i - 2] + nums[i - 1] + nums[i];
+        }
+        return 0;
+    }
+}
