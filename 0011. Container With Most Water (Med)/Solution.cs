ac3bcffcc14187a981c29b9531b7f62404cfe526
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-15 21:26:07 +0200

    Solved 11

---
+++ 0011. Container With Most Water (Med)/Solution.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int MaxArea(int[] heights)
+    {
+        int maxArea = 0;
+        int lower = 0, upper = heights.Length - 1;
+        while (lower < upper)
+        {
+            maxArea = Math.Max(maxArea, Math.Min(heights[lower], heights[upper]) * (upper - lower));
+            if (heights[lower] > heights[upper]) upper--;
+            else lower++;
+        }
+        return maxArea;
+    }
+}
