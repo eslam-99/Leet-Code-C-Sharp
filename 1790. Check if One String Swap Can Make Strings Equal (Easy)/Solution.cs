f098880fc3ef216bb10d11bcdc9a8097c53324b0
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-15 20:45:11 +0200

    Solved 1790

---
+++ 1790. Check if One String Swap Can Make Strings Equal (Easy)/Solution.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public bool AreAlmostEqual(string s1, string s2)
+    {
+        //int diff = 0;
+        List<char> chars = new(2);
+        bool found = false;
+        for (int i = 0; i < s1.Length; i++)
+        {
+            if (s1[i] != s2[i])
+            {
+                if (found) return false;
+                if (chars.Count != 0 && (chars[0] != s1[i] || chars[1] != s2[i])) return false;
+                else if (chars.Count == 0) chars = new() { s1[i], s2[i] };
+                else { found = true; chars = new(2); }
+            }
+        }
+        return chars.Count == 0;
+    }
+}
