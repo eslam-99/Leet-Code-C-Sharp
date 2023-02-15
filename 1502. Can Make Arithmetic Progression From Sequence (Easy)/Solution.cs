e345e1f8bf12b3fddb7ab20eccf42de0f2f7c49a
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-15 17:18:20 +0200

    Solved 1502

---
+++ 1502. Can Make Arithmetic Progression From Sequence (Easy)/Solution.cs
@@ -0,0 +1,11 @@
+﻿public class Solution
+{
+    public bool CanMakeArithmeticProgression(int[] arr)
+    {
+        Array.Sort(arr);
+        int diff = Math.Abs(arr[0] - arr[1]);
+        for (int i = 2; i < arr.Length; i++)
+            if (diff != Math.Abs(arr[i - 1] - arr[i])) return false;
+        return true;
+    }
+}
