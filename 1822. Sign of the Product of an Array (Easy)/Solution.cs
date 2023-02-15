c8a1f61e3759f694b84cf19048bb44a12e030ac3
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-15 17:12:24 +0200

    Solved 1822

---
+++ 1822. Sign of the Product of an Array (Easy)/Solution.cs
@@ -0,0 +1,9 @@
+﻿public class Solution
+{
+    public int ArraySign(int[] nums)
+    {
+        int prod = 1;
+        foreach (var n in nums) prod *= n.CompareTo(0);
+        return prod;
+    }
+}
