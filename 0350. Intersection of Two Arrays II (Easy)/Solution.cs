60ad73c5e109ae5d50661ae3858aac28ab3caf69
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-15 20:59:54 +0200

    Solved 350

---
+++ 0350. Intersection of Two Arrays II (Easy)/Solution.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int[] Intersect(int[] nums1, int[] nums2)
+    {
+        Dictionary<int, int> values = new();
+        List<int> result = new(Math.Min(nums1.Length, nums2.Length));
+
+        foreach (int n in nums1)
+            if (values.ContainsKey(n)) values[n]++;
+            else values.Add(n, 1);
+
+        foreach (int n in nums2)
+        {
+            if (values.ContainsKey(n) && values[n] > 0)
+            {
+                values[n]--;
+                result.Add(n);
+            }
+        }
+
+        return result.ToArray();
+    }
+}
