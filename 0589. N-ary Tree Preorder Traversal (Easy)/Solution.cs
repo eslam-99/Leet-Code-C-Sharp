3f9136ab84b5b9107e3f7b89a7ec8f57d9483923
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-16 14:02:49 +0200

    Solved 589

---
+++ 0589. N-ary Tree Preorder Traversal (Easy)/Solution.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public IList<int> Preorder(Node root)
+    {
+        return PreOrderTraversal(root).ToArray();
+    }
+
+    private IEnumerable<int> PreOrderTraversal(Node root)
+    {
+        if (root is null) yield break;
+        yield return root.val;
+        foreach (var c in root.children)
+            foreach (int n in PreOrderTraversal(c))
+                yield return n;
+
+    }
+}
