a1b2e8a8d8fc2cd3b5b977db5c693140d7547f04
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-16 16:28:56 +0200

    Solved 566

---
+++ 0566. Reshape the Matrix (Easy)/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution s = new();
+        s.MatrixReshape(new int[][] { new int[] { 1, 2}, new int[] { 3, 4} }, 4, 1);
+    }
+}
---
+++ 0566. Reshape the Matrix (Easy)/Solution.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public int[][] MatrixReshape(int[][] mat, int r, int c)
+    {
+        if (r == mat.Length || mat.Length * mat[0].Length != r * c) return mat;
+        int[][] result = new int[r][];
+        for (int i = 0; i < r; i++) result[i] = new int[c];
+        for (int i = 0; i < mat.Length * mat[0].Length; i++)
+            result[i / c][i % c] = mat[i / mat[0].Length][i % mat[0].Length];
+        return result;
+    }
+}
