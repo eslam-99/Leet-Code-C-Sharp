be434fd67c316a188a9b925fdb1b1509dd510b67
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-16 14:34:43 +0200

    Solved 496

---
+++ 0496. Next Greater Element I (Easy)/Solution.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int[] NextGreaterElement(int[] nums1, int[] nums2)
+    {
+        Dictionary<int, int> nums2Mapping = new();
+        int[] result = Enumerable.Repeat<int>(-1, nums1.Length).ToArray();
+
+        for (int i = 0; i < nums2.Length; i++) nums2Mapping[nums2[i]] = i;
+
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            int index = -1;
+            if (nums2Mapping.TryGetValue(nums1[i], out index))
+            {
+                for (int j = index + 1; j < nums2.Length; j++)
+                {
+                    if (nums1[i] < nums2[j])
+                    {
+                        result[i] = nums2[j];
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
