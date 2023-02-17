dc8b64c7d8ff279de74a3d6d66f8087a969d4b0f
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-17 20:33:17 +0200

    Solved 283

---
+++ 0283. Move Zeroes (Easy)/Solution.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int nextZeroIndex = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                (nums[i], nums[nextZeroIndex]) = (nums[nextZeroIndex++], nums[i]);
+            }
+        }
+    }
+}
