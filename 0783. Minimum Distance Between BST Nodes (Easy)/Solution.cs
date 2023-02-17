3b5520873b01a305e47c7e78fc99196c34a10c24
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-18 00:07:04 +0200

    Solved 783

---
+++ 0783. Minimum Distance Between BST Nodes (Easy)/Solution.cs
@@ -0,0 +1,21 @@
+﻿class Solution
+{
+    int minDifference = int.MaxValue;
+    TreeNode? prevValue;
+
+    void InorderTraversal(TreeNode root)
+    {
+        if (root == null) return;
+        InorderTraversal(root.left);
+        if (prevValue != null)
+            minDifference = Math.Min(minDifference, root.val - prevValue.val);
+        prevValue = root;
+        InorderTraversal(root.right);
+    }
+
+    public int MinDiffInBST(TreeNode root)
+    {
+        InorderTraversal(root);
+        return minDifference;
+    }
+}
