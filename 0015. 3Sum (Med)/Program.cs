a1491cc0a04acafdad6e4be6eceef73974616670
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-18 00:00:33 +0200

    Solved 15

---
+++ 0015. 3Sum (Med)/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution s = new();
+        s.ThreeSum(new[] { -1,0,1,2,-1,-4 });
+    }
+}
---
+++ 0015. 3Sum (Med)/Solution.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        HashSet<(int, int, int)> result = new();
+        Array.Sort(nums);
+        for (int lower = 0; lower < nums.Length - 2; lower++)
+        {
+            int middle = lower + 1, upper = nums.Length - 1;
+            while (upper > middle)
+            {
+                int sum = nums[lower] + nums[middle] + nums[upper];
+                if (sum == 0)
+                {
+                    result.Add((nums[lower], nums[middle], nums[upper]));
+                    middle++;
+                    upper--;
+                }
+                else if (sum < 0) middle++;
+                else upper--;
+            }
+        }
+        return result.Select(x => new[] { x.Item1, x.Item2, x.Item3 }).ToArray();
+    }
+}
