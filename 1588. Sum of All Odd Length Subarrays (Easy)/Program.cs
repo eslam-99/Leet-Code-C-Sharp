9ca9308efee4e8b985951460b27420c09377ae4f
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-17 20:32:50 +0200

    Solved 1588

---
+++ 1588. Sum of All Odd Length Subarrays (Easy)/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution s = new();
+        Console.WriteLine(s.SumOddLengthSubarrays(new int[] { 1, 4, 2, 5, 3 }));
+        Console.WriteLine(s.SumOddLengthSubarrays(new int[] { 1, 1, 1, 1, 1, 1, 1 }));
+        Console.WriteLine(s.SumOddLengthSubarrays(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 }));
+        Console.WriteLine(s.SumOddLengthSubarrays(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }));
+    }
+}
---
+++ 1588. Sum of All Odd Length Subarrays (Easy)/Solution.cs
@@ -0,0 +1,75 @@
+﻿public class Solution
+{
+    public int SumOddLengthSubarrays(int[] arr)
+    {
+        int n = (arr.Length + 1) / 2, sum = 0;
+        for (int i = 0 ; i < n ; i++)
+        {
+            int c = 1, left = 0, right = arr.Length - 1;
+            int windowSize = i * 2 + 1;
+            while (left < right)
+            {
+                int repeatition = Math.Min(c, Math.Min(arr.Length + 1 - windowSize, windowSize));
+                sum += arr[left++] * repeatition + arr[right--] * repeatition;
+                c++;
+            }
+            if (left == right)
+            {
+                int repeatition = Math.Min(c, Math.Min(arr.Length + 1 - windowSize, windowSize));
+                sum += arr[left] * repeatition;
+            }
+        }
+        return sum;
+    }
+}
+
+// 
+// A B C D E F G F E D C B A
+// 1 1 1 1 1 1 1 1 1 1 1 1 1 : 1
+// 1 2 3 3 3 3 3 3 3 3 3 2 1 : 3
+// 1 2 3 4 5 5 5 5 5 4 3 2 1 : 5
+// 1 2 3 4 5 6 7 6 5 4 3 2 1 : 7
+// 1 2 3 4 5 5 5 5 5 4 3 2 1 : 9
+// 1 2 3 3 3 3 3 3 3 3 3 2 1 : 11
+// 1 1 1 1 1 1 1 1 1 1 1 1 1 : 13
+// 
+// A B C D E F F E D C B A
+// 1 1 1 1 1 1 1 1 1 1 1 1 : 1
+// 1 2 3 3 3 3 3 3 3 3 2 1 : 3
+// 1 2 3 4 5 5 5 5 4 3 2 1 : 5
+// 1 2 3 4 5 6 6 5 4 3 2 1 : 7
+// 1 2 3 4 4 4 4 4 4 4 2 1 : 9
+// 1 1 1 1 1 1 1 1 1 1 1 1 : 11
+// 
+// A B C D E F E D C B A
+// 1 1 1 1 1 1 1 1 1 1 1 : 1
+// 1 2 3 3 3 3 3 3 3 2 1 : 3
+// 1 2 3 4 5 5 5 4 3 2 1 : 5
+// 1 2 3 4 5 5 5 4 3 2 1 : 7
+// 1 2 3 3 3 3 3 3 3 2 1 : 9
+// 1 1 1 1 1 1 1 1 1 1 1 : 11
+// 
+// A B C D E D C B A
+// 1 1 1 1 1 1 1 1 1 : 1
+// 1 2 3 3 3 3 3 2 1 : 3
+// 1 2 3 4 5 4 3 2 1 : 5
+// 1 2 3 3 3 3 3 2 1 : 7
+// 1 1 1 1 1 1 1 1 1 : 9
+// 
+// A B C D C B A
+// 1 1 1 1 1 1 1 : 1
+// 1 2 3 3 3 2 1 : 3
+// 1 2 3 3 3 2 1 : 5
+// 1 1 1 1 1 1 1 : 7
+// 
+// A B C B A
+// 1 1 1 1 1 : 1
+// 1 2 3 2 1 : 3
+// 1 1 1 1 1 : 5
+// 
+// 
+// 
+// 
+// 
+// 
+// 
