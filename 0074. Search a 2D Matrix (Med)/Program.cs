181ec5fc436543b984afc46a7f12024797bdbc5f
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-17 23:25:06 +0200

    Solved 74

---
+++ 0074. Search a 2D Matrix (Med)/Program.cs
@@ -0,0 +1,62 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution s = new();
+        //Console.WriteLine(s.SearchMatrix(
+        //    new int[][]
+        //    {
+        //        new int[] {1, 3, 5, 7 },
+        //        new int[] {10, 11, 16, 20},
+        //        new int[] {23, 30, 34, 60}
+        //    },
+        //    3
+        //));
+        //Console.WriteLine(s.SearchMatrix(
+        //    new int[][]
+        //    {
+        //        new int[] {1, 3, 5, 7},
+        //        new int[] {10, 11, 16, 20},
+        //        new int[] { 23, 30, 34, 60 }
+        //    },
+        //    13
+        //));
+        //Console.WriteLine(s.SearchMatrix(
+        //    new int[][]
+        //    {
+        //        new int[] {1}
+        //    },
+        //    1
+        //));
+        //Console.WriteLine(s.SearchMatrix(
+        //    new int[][]
+        //    {
+        //        new int[] {1, 1}
+        //    },
+        //    0
+        //));
+        //Console.WriteLine(s.SearchMatrix(
+        //    new int[][]
+        //    {
+        //        new int[] {1, 1}
+        //    },
+        //    2
+        //));
+        //Console.WriteLine(s.SearchMatrix(
+        //    new int[][]
+        //    {
+        //        new int[] {1},
+        //        new int[] {3}
+        //    },
+        //    3
+        //));
+        Console.WriteLine(s.SearchMatrix(
+            new int[][]
+            {
+                new int[] {1, 1},
+                new int[] {2, 2}
+            },
+            3
+        ));
+    }
+}
---
+++ 0074. Search a 2D Matrix (Med)/Solution.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public bool SearchMatrix(int[][] matrix, int target)
+    {
+        int rows = matrix.Length, cols = matrix[0].Length;
+        int len = rows * cols;
+        int left = 0, right = len - 1;
+        int current = (right + left) >> 1;
+        while (left <= right)
+        {
+            int row = current / cols, col = current % cols;
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] > target) right = current - 1;
+            else left = current + 1;
+            current = (right + left) >> 1;
+        }
+        return false;
+    }
+}
