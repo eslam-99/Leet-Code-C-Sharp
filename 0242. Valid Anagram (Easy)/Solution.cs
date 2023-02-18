85e5f0a4f98ba8f5e61776bb1e06bfbe008235c7
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-18 16:13:52 +0200

    Solved 242

---
+++ 0242. Valid Anagram (Easy)/Solution.cs
@@ -0,0 +1,11 @@
+﻿public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length) return false;
+        uint[] chars = new uint[26];
+        foreach (char c in s) chars[c - 'a']++;
+        foreach (char c in t) if (chars[c - 'a']-- == 0) return false;
+        return true;
+    }
+}
