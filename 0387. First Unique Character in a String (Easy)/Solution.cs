eb861fe17c8ce4b0fd3be8f9c739a40efb45f766
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-18 15:15:08 +0200

    Solved 387

---
+++ 0387. First Unique Character in a String (Easy)/Solution.cs
@@ -0,0 +1,10 @@
+﻿public class Solution
+{
+    public int FirstUniqChar(string s)
+    {
+        int[] chars = new int[26];
+        foreach (char c in s) chars[c - 'a'] += 1;
+        for (int i = 0; i < s.Length; i++) if (chars[s[i] - 'a'] == 1) return i;
+        return -1;
+    }
+}
