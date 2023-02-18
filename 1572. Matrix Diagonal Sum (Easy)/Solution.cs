0ca094acc8f536640c7df2a9f871b2d52d052375
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-18 14:54:42 +0200

    Solved 1572

---
+++ 1572. Matrix Diagonal Sum (Easy)/Solution.cs
@@ -0,0 +1,13 @@
+﻿public class Solution
+{
+    public int DiagonalSum(int[][] mat)
+    {
+        int sum = 0;
+        for (int i = 0; i < (mat.Length + 1) / 2; i++)
+        {
+            if (i == (mat[0].Length + 1) / 2 - 1 && mat[0].Length % 2 == 1) sum += mat[i][i];
+            else sum += mat[i][i] + mat[^(i + 1)][^(i + 1)] + mat[^(i + 1)][i] + mat[i][^(i + 1)];
+        }
+        return sum;
+    }
+}
