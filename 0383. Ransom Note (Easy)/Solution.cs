faaa2d8408919d468d3247ba64173e2e67d4d81c
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-18 16:10:00 +0200

    Solved 383

---
+++ 0383. Ransom Note (Easy)/Solution.cs
@@ -0,0 +1,10 @@
+﻿public class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        uint[] chars = new uint[26];
+        foreach (char c in magazine) chars[c - 'a']++;
+        foreach (char c in ransomNote) if (chars[c - 'a']-- == 0) return false;
+        return true;
+    }
+}
