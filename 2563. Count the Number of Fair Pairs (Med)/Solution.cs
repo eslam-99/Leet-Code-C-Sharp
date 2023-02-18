7128dafc18e91a844f0b8daafbd216df386efc2d
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-18 20:11:06 +0200

    Solved 2563

---
+++ 2563. Count the Number of Fair Pairs (Med)/Solution.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public long CountFairPairs(int[] nums, int lower, int upper)
+    {
+        Array.Sort(nums);
+        //   lower                    upper
+        //     *                        *
+        // ******************************....
+        // MINUS
+        // ****..............................
+        return GetRange(nums, upper) - GetRange(nums, lower - 1);
+    }
+
+    private long GetRange(int[] nums, int upper)
+    {
+        long res = 0;
+        for (int i = 0, j = nums.Length - 1; i < j; ++i)
+        {
+            while (i < j && nums[i] + nums[j] > upper) --j;
+            res += j - i;
+        }
+        return res;
+    }
+}
