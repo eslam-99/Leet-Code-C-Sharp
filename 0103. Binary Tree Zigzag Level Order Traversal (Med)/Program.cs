ccecd944d2856c613df8b6183ae40ed979c37efc
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-19 20:31:23 +0200

    Solved 103

---
+++ 0103. Binary Tree Zigzag Level Order Traversal (Med)/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.Json;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution s = new();
+        IList<IList<int>> result = new List<IList<int>>();
+
+        result = s.ZigzagLevelOrder(
+            new TreeNode(
+                3,
+                new TreeNode(9),
+                new TreeNode(20,
+                    new TreeNode(15),
+                    new TreeNode(7)
+                )
+            )
+        );
+        Console.WriteLine("[" + string.Join(',', result.Select(i => " [ " + string.Join(',', i) + " ] ")) + "]");
+
+        result = s.ZigzagLevelOrder(
+            new TreeNode( 1 )
+        );
+        Console.WriteLine("[" + string.Join(',', result.Select(i => " [ " + string.Join(',', i) + " ] ")) + "]");
+
+        result = s.ZigzagLevelOrder( null!);
+        Console.WriteLine("[" + string.Join(',', result.Select(i => " [ " + string.Join(',', i) + " ] ")) + "]");
+
+        result = s.ZigzagLevelOrder(
+            new TreeNode(
+                1,
+                new TreeNode(2,
+                    new TreeNode(4)
+                ),
+                new TreeNode(3,
+                    null!,
+                    new TreeNode(5)
+                )
+            )
+        );
+        Console.WriteLine("[" + string.Join(',', result.Select(i => " [ " + string.Join(',', i) + " ] ")) + "]");
+        
+        result = s.ZigzagLevelOrder(
+            new TreeNode(
+                0,
+                new TreeNode(2,
+                    new TreeNode(1,
+                        new TreeNode(5),
+                        new TreeNode(1)
+                    )
+                ),
+                new TreeNode(4,
+                    new TreeNode(3,
+                        null!,
+                        new TreeNode(6)
+                    ),
+                    new TreeNode(-1,
+                        null!,
+                        new TreeNode(8)
+                    )
+                )
+            )
+        );
+        Console.WriteLine("My Answer    : [" + string.Join(',', result.Select(i => "[" + string.Join(',', i) + "]")) + "]");
+        Console.WriteLine("Right Answer : [[0],[4,2],[1,3,-1],[8,6,1,5]]");
+    }
+}
---
+++ 0103. Binary Tree Zigzag Level Order Traversal (Med)/Solution.cs
@@ -0,0 +1,50 @@
+﻿public class Solution
+{
+    public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+    {
+        if (root is null) return new List<IList<int>>();
+        return new List<IList<int>>() { new List<int> { root.val } }.Concat(BFS(root, true)).ToList();
+    }
+
+    private List<List<int>> BFS(TreeNode root, bool reversed)
+    {
+        if (root == null) return null!;
+        List<List<int>> result = new();
+        List<List<int>> leftTree = new();
+        List<List<int>> rightTree = new();
+        List<int> children = new();
+        if (root.left is not null)
+        {
+            children.Add(root.left.val);
+            leftTree = BFS(root.left, !reversed);
+        }
+        if (root.right is not null)
+        {
+            children.Add(root.right.val);
+            rightTree = BFS(root.right, !reversed);
+        }
+        if (children.Count > 0) result.Add(reversed ? Enumerable.Reverse(children).ToList() : children);
+        if (leftTree.Count > 0 && rightTree.Count > 0)
+        {
+            for (int i = 0; i < Math.Max(leftTree.Count, rightTree.Count); i++)
+            {
+                if (i < leftTree.Count && i < rightTree.Count)
+                {
+                    if (reversed && i % 2 == 1)
+                        result.Add((leftTree[i].Concat(rightTree[i]).Reverse()).ToList());
+                        //result.Add(rightTree[i].Concat(leftTree[i]).ToList());
+                    else
+                        result.Add(leftTree[i].Concat(rightTree[i]).ToList());
+                }
+                else if (i < leftTree.Count) result.Add(leftTree[i]);
+                else if (i < rightTree.Count) result.Add(rightTree[i]);
+            }
+        }
+        else if (leftTree.Count > 0)
+            for (int i = 0; i < leftTree.Count; i++) result.Add(leftTree[i]);
+        else if (rightTree.Count > 0)
+            for (int i = 0; i < rightTree.Count; i++) result.Add(rightTree[i]);
+
+        return result;
+    }
+}
