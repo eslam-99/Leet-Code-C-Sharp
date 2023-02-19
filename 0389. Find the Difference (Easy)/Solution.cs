14fb772b59eff614545c3314b7804e43794863f8
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-19 19:13:07 +0200

    Solved 389

---
+++ 0389. Find the Difference (Easy)/Solution.cs
@@ -0,0 +1,9 @@
+﻿public class Solution
+{
+    public char FindTheDifference(string s, string t)
+    {
+        int diff = 0;
+        foreach (char c in s + t) diff ^= c;
+        return (char)diff;
+    }
+}
