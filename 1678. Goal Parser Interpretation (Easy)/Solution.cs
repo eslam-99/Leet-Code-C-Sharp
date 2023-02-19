68d84e4e53c555b14b1ae26694283ae50b97311a
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-19 19:04:35 +0200

    Solved 1678

---
+++ 1678. Goal Parser Interpretation (Easy)/Solution.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public string Interpret(string command)
+    {
+        string result = "";
+        for (int i = 0; i < command.Length; i++)
+        {
+
+            switch (command[i])
+            {
+                case 'G': result += "G"; break;
+
+                case '(':
+                    if (command[i + 1] == ')')
+                    {
+                        result += "o";
+                        i += 1;
+                    }
+                    else
+                    {
+                        result += "al";
+                        i += 3;
+                    }
+                    break;
+            }
+        }
+        return result;
+    }
+}
