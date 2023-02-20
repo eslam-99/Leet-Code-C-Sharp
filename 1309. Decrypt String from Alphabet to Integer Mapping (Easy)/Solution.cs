1552a1ae5f36245adc16bac52120c6392b8ee757
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-20 14:49:58 +0200

    Solved 1309

---
+++ 1309. Decrypt String from Alphabet to Integer Mapping (Easy)/Solution.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public string FreqAlphabets(string s)
+    {
+        string result = "", temp = "";
+        foreach (char c in s)
+        {
+            if (temp.Length == 3)
+            {
+                result += MapChar(temp[0]);
+                temp = temp[1..];
+            }
+            switch (c)
+            {
+                case '#':
+                    result += MapChar(temp);
+                    temp = "";
+                    break;
+                default: temp += c; break;
+            }
+        }
+        foreach (char c in temp) result += MapChar(c);
+        return result;
+    }
+
+    private char MapChar(char c) => (char)(c - '1' + 'a');
+    private char MapChar(string s) => (char)(int.Parse(s) + 'a' - 1);
+}
