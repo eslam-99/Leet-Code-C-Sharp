c07ba738ed36f32e9c7c658266937f280af970ab
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-20 14:23:37 +0200

    Solved 709

---
+++ 0709. To Lower Case (Easy)/Solution.cs
@@ -0,0 +1,11 @@
+﻿public class Solution
+{
+    public string ToLowerCase(string s)
+    {
+        string result = "";
+        for (int i = 0; i < s.Length; i++)
+            if (s[i] >= 65 && s[i] <= 90) result += (char)(s[i] + 32);
+            else result += s[i];
+        return result;
+    }
+}
