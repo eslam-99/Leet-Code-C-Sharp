bd9e41be696a18c02468f8d83bd52cdfcdd85630
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-21 20:37:34 +0200

    Solved 1290

---
+++ 1290. Convert Binary Number in a Linked List to Integer (Easy)/Solution.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int GetDecimalValue(ListNode head)
+    {
+        int result = 0, level = 0;
+        ListNode temp = head;
+        while (temp is not null)
+        {
+            temp = temp.next;
+            level++;
+        }
+        while (head is not null)
+        {
+            result += (int)Math.Pow(2, --level) * head.val;
+            head = head.next;
+        }
+        return result;
+    }
+}
