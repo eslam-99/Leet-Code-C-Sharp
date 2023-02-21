cbb8759f28be1c5800a24f2e21f3b8e7c1bd5ee4
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-21 19:13:29 +0200

    Solved 404

---
+++ 0404. Sum of Left Leaves (Easy)/Solution.cs
@@ -0,0 +1,10 @@
+﻿public class Solution
+{
+    public int SumOfLeftLeaves(TreeNode root)
+    {
+        if (root is null) return 0;
+        return SumOfLeftLeaves(root.left) + SumOfLeftLeaves(root.right) +
+            (root.left is not null && root.left.left is null && root.left.right is null
+            ? root.left.val : 0);
+    }
+}
