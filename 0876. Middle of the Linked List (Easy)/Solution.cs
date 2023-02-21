c17b93dc318b4f4533612d2c3528dfc7df791c5d
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-21 18:23:50 +0200

    Solved 876

---
+++ 0876. Middle of the Linked List (Easy)/Solution.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public ListNode MiddleNode(ListNode head)
+    {
+        if (head is null) return null!;
+        ListNode slow = head, fast = head;
+        while (fast.next is not null)
+        {
+            fast = fast.next;
+            slow = slow.next;
+            if (fast.next is not null) fast = fast.next;
+        }
+        return slow;
+    }
+}
