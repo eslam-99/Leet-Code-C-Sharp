7162cb974adb9bcfabcb08b41c4df637800a1513
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-22 20:49:32 +0200

    Solved 1011

---
+++ 1011. Capacity To Ship Packages Within D Days (Med)/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution s = new();
+        Console.WriteLine(s.ShipWithinDays(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 5) + " : 15");
+        Console.WriteLine(s.ShipWithinDays(new int[] { 3, 2, 2, 4, 1, 4 }, 3) + " : 6");
+        Console.WriteLine(s.ShipWithinDays(new int[] { 1, 2, 3, 1, 1 }, 4) + " : 3");
+        Console.WriteLine(s.ShipWithinDays(new int[] { 2, 2, 3, 4, 4, 5 }, 2) + " : 11");
+    }
+}
---
+++ 1011. Capacity To Ship Packages Within D Days (Med)/Solution.cs
@@ -0,0 +1,37 @@
+﻿public class Solution
+{
+    public int ShipWithinDays(int[] weights, int days)
+    {
+        int maxLoad = -1, totalWeight = 0;
+        foreach (int w in weights)
+        {
+            totalWeight += w;
+            maxLoad = Math.Max(maxLoad, w);
+        }
+
+        int left = maxLoad, right = totalWeight;
+        while (left < right)
+        {
+            int mid = (left + right) / 2;
+            if (IsFeasible(weights, mid, days)) right = mid;
+            else left = mid + 1;
+        }
+        return left;
+    }
+
+    private bool IsFeasible(int[] weights, int capacity, int days)
+    {
+        int currentDays = 1, currentCapacity = 0;
+        foreach (int w in weights)
+        {
+            currentCapacity += w;
+            if (currentCapacity > capacity)
+            {
+                currentCapacity = w;
+                currentDays++;
+            }
+            if (currentDays > days) return false;
+        }
+        return currentDays <= days;
+    }
+}
