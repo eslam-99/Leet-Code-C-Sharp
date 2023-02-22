9c234f8cffba04430c830be5bf5a08a1b8fd5b21
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-22 19:08:14 +0200

    Solved 1356

---
+++ 1356. Sort Integers by The Number of 1 Bits (Easy)/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution s = new();
+        Console.WriteLine(string.Join(',', s.SortByBits(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 })));
+        Console.WriteLine(string.Join(',', s.SortByBits(new int[] { 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1 })));
+    }
+}
---
+++ 1356. Sort Integers by The Number of 1 Bits (Easy)/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+
+public class Solution
+{
+    public int[] SortByBits(int[] arr)
+    {
+        return SortByBits_V2(arr);
+    }
+    public int[] SortByBits_V1(int[] arr)
+    {
+        SortedList<int, List<int>> numbers = new();
+        int[] result = new int[arr.Length];
+        foreach (int n in arr)
+        {
+            int c = BitOperations.PopCount((uint)n);
+            if (numbers.ContainsKey(c)) numbers[c].Add(n);
+            else numbers[c] = new() { n };
+        }
+        int i = 0;
+        foreach (var pair in numbers)
+        {
+            pair.Value.Sort();
+            foreach (int n in pair.Value) result[i++] = n;
+        }
+
+        return result;
+    }
+    public int[] SortByBits_V2(int[] arr)
+    {
+        return arr.OrderBy(x => (BitOperations.PopCount((uint)x), x)).ToArray();
+    }
+}
