39b7880f1e8c44c66a21d4acd30e3fee760d1f84
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-23 13:51:43 +0200

    Solved 1603

---
+++ 1603. Design Parking System (Easy)/ParkingSystem.cs
@@ -0,0 +1,20 @@
+﻿public class ParkingSystem
+{
+
+    private int[] Lots { get; set; }
+    
+    public ParkingSystem(int big, int medium, int small)
+    {
+        Lots = new int[] { big, medium, small };
+    }
+    
+    public bool AddCar(int carType)
+    {
+        if (Lots[carType - 1] > 0)
+        {
+            Lots[carType - 1]--;
+            return true;
+        }
+        return false;
+    }
+}
