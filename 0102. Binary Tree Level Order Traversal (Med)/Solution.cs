e90748b1ed6a9c58ea6030fbecf3d384556e93bf
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-23 14:07:02 +0200

    Updated 102

--- 0102. Binary Tree Level Order Traversal (Med)/Solution.cs
+++ 0102. Binary Tree Level Order Traversal (Med)/Solution.cs
@@ -1,46 +1,20 @@
 ﻿public class Solution
 {
+    private List<IList<int>> levels = new List<IList<int>>();
     public IList<IList<int>> LevelOrder(TreeNode root)
     {
-        if (root is null) return new List<IList<int>>();
-        return new List<IList<int>>() { new List<int> { root.val } }.Concat(BFS(root)).ToList();
+        if (root == null) return levels;
+        BFS(root, 0);
+        return levels;
     }
 
-    private List<List<int>> BFS(TreeNode root)
+    public void BFS(TreeNode node, int level)
     {
-        if (root == null) return null!;
-        List<List<int>> result = new();
-        List<List<int>> leftTree = new();
-        List<List<int>> rightTree = new();
-        List<int> children = new();
-        if (root.left is not null)
-        {
-            children.Add(root.left.val);
-            leftTree = BFS(root.left);
-        }
-        if (root.right is not null)
-        {
-            children.Add(root.right.val);
-            rightTree = BFS(root.right);
-        }
-        if (children.Count > 0) result.Add(children);
-        if (leftTree.Count > 0 && rightTree.Count > 0)
-        {
-            for (int i = 0; i < Math.Max(leftTree.Count, rightTree.Count); i++)
-            {
-                if (i < leftTree.Count && i < rightTree.Count)
-                {
-                    result.Add(leftTree[i].Concat(rightTree[i]).ToList());
-                }
-                else if (i < leftTree.Count) result.Add(leftTree[i]);
-                else if (i < rightTree.Count) result.Add(rightTree[i]);
-            }
-        }
-        else if (leftTree.Count > 0)
-            for (int i = 0; i < leftTree.Count; i++) result.Add(leftTree[i]);
-        else if (rightTree.Count > 0)
-            for (int i = 0; i < rightTree.Count; i++) result.Add(rightTree[i]);
+        if (levels.Count == level) levels.Add(new List<int>());
 
-        return result;
+        levels[level].Add(node.val);
+
+        if (node.left != null) BFS(node.left, level + 1);
+        if (node.right != null) BFS(node.right, level + 1);
     }
 }
