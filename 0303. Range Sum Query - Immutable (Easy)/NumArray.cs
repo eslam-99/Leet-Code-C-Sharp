d17f11f6c90fc965cdcac1feebe45b02d4ddd7f8
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-23 13:56:07 +0200

    Solved 303

---
+++ 0303. Range Sum Query - Immutable (Easy)/NumArray.cs
@@ -0,0 +1,13 @@
+﻿public class NumArray
+{
+    private int[] Nums { get; set; }
+    public NumArray(int[] nums)
+    {
+        Nums = nums;
+    }
+
+    public int SumRange(int left, int right)
+    {
+        return Nums[left..(right + 1)].Sum();
+    }
+}
