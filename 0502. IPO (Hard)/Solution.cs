acb9181e7a94238df7e0683212c12ef95168bf28
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-23 21:49:52 +0200

    Solved 502

---
+++ 0502. IPO (Hard)/Solution.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public int FindMaximizedCapital(int k, int w, int[] profits, int[] capital)
+    {
+        // negative profit to sort descening
+        // capital
+        List<(int, int)> list = new();
+        for (int i = 0; i < profits.Length; i++)
+            list.Add((-profits[i], capital[i]));
+        list.Sort();
+        for (int i = 0; i < list.Count; i++)
+        {
+            int j = 0;
+            foreach ((int, int) proj in list)
+            {
+                if (proj.Item2 <= w)
+                {
+                    w += -proj.Item1;
+                    list.Remove(proj);
+                    j--;
+                    break;
+                }
+                j++;
+            }
+            if (j == list.Count) break;
+        }
+
+        return w;
+    }
+}
