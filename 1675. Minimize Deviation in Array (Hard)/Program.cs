3422c7c5b403c2c222ce70f7a737ccadf24c48d0
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-24 23:16:43 +0200

    Solved 1675

---
+++ 1675. Minimize Deviation in Array (Hard)/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution s = new();
+        Console.WriteLine(s.MinimumDeviation(new int[] { 1, 2, 3, 4 }) + " : 1");
+        Console.WriteLine(s.MinimumDeviation(new int[] { 4, 1, 5, 20, 3 }) + " : 3");
+        Console.WriteLine(s.MinimumDeviation(new int[] { 2, 10, 8 }) + " : 3");
+        Console.WriteLine(s.MinimumDeviation(new int[] { 4, 9, 4, 5 }) + " : 5");
+        Console.WriteLine(s.MinimumDeviation(new int[] { 2, 8, 6, 1, 6 }) + " : 1");
+        Console.WriteLine(s.MinimumDeviation(new int[] { 10, 4, 3 }) + " : 2");
+    }
+}
---
+++ 1675. Minimize Deviation in Array (Hard)/Solution.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int MinimumDeviation(int[] nums)
+    {
+        PriorityQueue<int, int> pq = new(Comparer<int>.Create((x, y) => y - x));
+        int minValue = int.MaxValue, minDiviation = int.MaxValue;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] % 2 == 1) nums[i] <<= 1;
+            pq.Enqueue(nums[i], nums[i]);
+            minValue = Math.Min(minValue, nums[i]);
+        }
+
+        while (true)
+        {
+            int maxValue = pq.Dequeue();
+            minDiviation = Math.Min(minDiviation, maxValue - minValue);
+            if (maxValue % 2 == 1) break;
+            maxValue >>= 1;
+            minValue = Math.Min(minValue, maxValue);
+            pq.Enqueue(maxValue, maxValue);
+        }
+
+        return minDiviation;
+    }
+}
