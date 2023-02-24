0acf62be185bd5f23b403d28a8dbfbf0d6a798e2
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-24 16:49:49 +0200

    Solved 704

---
+++ 0704. Binary Search (Easy)/Solution.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int left = 0, right = nums.Length;
+        while (left < right)
+        {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) left = mid + 1;
+            else right = mid;
+        }
+        return -1;
+    }
+}
