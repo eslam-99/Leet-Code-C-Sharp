80c8efe02a6bac07fda1767e7f5e8607c3405b62
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-25 13:59:40 +0200

    Solved 189

---
+++ 0189. Rotate Array (Med)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution s = new();
+        
+        int[] nums = new int[]{ 1, 2, 3, 4, 5, 6, 7 };
+        s.Rotate(nums, 3);
+        Console.WriteLine(string.Join(',', nums));
+        
+        nums = new int[]{ -1, -100, 3, 99 };
+        s.Rotate(nums, 2);
+        Console.WriteLine(string.Join(',', nums));
+
+        nums = new int[]{ 1, 2, 3, 4, 5, 6 };
+        s.Rotate(nums, 3);
+        Console.WriteLine(string.Join(',', nums));
+    }
+}
---
+++ 0189. Rotate Array (Med)/Solution.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public void Rotate(int[] nums, int k)
+    {
+        if (k > nums.Length) k %= nums.Length;
+        if (k == 0 || k == nums.Length) return;
+        int swaps = 0;
+        for (int i = 0; swaps < nums.Length - 1; i++)
+        {
+            int pos = i;
+            int n = nums[^(k - i)];
+            do
+            {
+                (n, nums[pos]) = (nums[pos], n);
+                pos = (pos + k) % nums.Length;
+                swaps++;
+            } while (pos != i);
+        }
+    }
+}
