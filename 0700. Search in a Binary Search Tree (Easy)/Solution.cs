92eca03ff2a27202a6d1dd9fb1562799f2448bde
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-25 11:44:21 +0200

    Solved 700

---
+++ 0700. Search in a Binary Search Tree (Easy)/Solution.cs
@@ -0,0 +1,10 @@
+﻿public class Solution
+{
+    public TreeNode SearchBST(TreeNode root, int val)
+    {
+        if (root is null) return null!;
+        if (root.val == val) return root;
+        else if (root.val > val) return SearchBST(root.left, val);
+        else return SearchBST(root.right, val);
+    }
+}
