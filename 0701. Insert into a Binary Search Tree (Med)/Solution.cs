c79ba4c884bd9035ad0a0a62591ee8f27d34268d
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-25 12:00:03 +0200

    Solved 701

---
+++ 0701. Insert into a Binary Search Tree (Med)/Solution.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public TreeNode InsertIntoBST(TreeNode root, int val)
+    {
+        if (root is null) return new TreeNode(val);
+        TreeNode ptr = root;
+        while (true)
+        {
+
+            if (ptr.val > val)
+            {
+                if (ptr.left is null)
+                {
+                    ptr.left = new(val);
+                    break;
+                }
+                else ptr = ptr.left;
+            }
+            else
+            {
+                if (ptr.right is null)
+                {
+                    ptr.right = new(val);
+                    break;
+                }
+                else ptr = ptr.right;
+            }
+        }
+        return root;
+    }
+}
