1f53425822e234e2c152b914e92664c13a1004c0
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-26 17:35:59 +0200

    Solved 235

---
+++ 0235. Lowest Common Ancestor of a Binary Search Tree (Med)/Solution.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if (root is null) return null!;
+        if (root.val < p.val && root.val < q.val)
+            return LowestCommonAncestor(root.right, p, q);
+        else if (root.val > p.val && root.val > q.val)
+            return LowestCommonAncestor(root.left, p, q);
+        return root;
+    }
+}
---
+++ 0235. Lowest Common Ancestor of a Binary Search Tree (Med)/TreeNode.cs
@@ -0,0 +1,6 @@
+﻿public class TreeNode {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+     public TreeNode(int x) { val = x; }
+ }
