c2a9bb50e7d89e76345e6312af047e735b85466f
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-26 16:53:34 +0200

    Solved 98

---
+++ 0098. Validate Binary Search Tree (Med)/Solution.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public bool IsValidBST(TreeNode root)
+    {
+        if (root is null) return true;
+
+        return Isbst(root, long.MinValue, long.MaxValue);
+    }
+
+    bool Isbst(TreeNode root, long min, long max)
+    {
+        if (root is null) return true;
+
+        if (root.val > min && root.val < max)
+            return Isbst(root.left, min, root.val)
+                && Isbst(root.right, root.val, max);
+
+        return false;
+    }
+}
