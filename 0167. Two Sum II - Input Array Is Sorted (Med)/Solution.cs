0cb25ae58243482a598e2fc28892548c379917d1
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-26 17:44:56 +0200

    Solved 167

---
+++ 0167. Two Sum II - Input Array Is Sorted (Med)/Solution.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        int left = 0, right = numbers.Length - 1;
+        while (left < right)
+        {
+            if (numbers[left] == target - numbers[right]) break;
+            else if (numbers[left] < target - numbers[right]) left++;
+            else right--;
+        }
+        return new int[] { left, right };
+    }
+}
