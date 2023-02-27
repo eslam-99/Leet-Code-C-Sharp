11cb14dcc76ad2d8a9f4b63d8fba5be531056595
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-27 20:12:13 +0200

    Solved 2

---
+++ 0002. Add Two Numbers (Med)/Solution.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode root = new(), iter = root;
+        bool overflow = false;
+        while (l1 is not null || l2 is not null || overflow)
+        {
+            int sum = 0;
+            if (l1 is not null) { sum += l1.val; l1 = l1.next; }
+            if (l2 is not null) { sum += l2.val; l2 = l2.next; }
+            if (overflow) sum++;
+            overflow = false;
+            if (sum > 9)
+            {
+                sum %= 10;
+                overflow = true;
+            }
+            iter.next = new(sum);
+            iter = iter.next;
+        }
+        return root.next;
+    }
+}
