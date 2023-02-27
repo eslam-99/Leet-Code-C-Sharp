1c26145cdbbe578696c503aee4017264ee5186ed
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-27 20:47:06 +0200

    Solved 3

---
+++ 0003. Longest Substring Without Repeating Characters (Med)/Solution.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        Dictionary<char, int> substring = new();
+        int maxLen = 0, currentLen = 0;
+        int i = 0;
+        foreach (char c in s)
+        {
+            if (!substring.ContainsKey(c))
+            {
+                substring[c] = i;
+                currentLen++;
+            }
+            else
+            {
+                maxLen = Math.Max(maxLen, currentLen);
+                foreach (var p in substring)
+                    if (p.Value < substring[c]) substring.Remove(p.Key);
+                substring[c] = i;
+                currentLen = substring.Count;
+            }
+            i++;
+        }
+        return Math.Max(maxLen, currentLen);
+    }
+}
