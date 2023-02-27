9d5cef00be1e2b9752378c380483244c7dc7fb1a
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-27 16:27:51 +0200

    Solved 557

---
+++ 0557. Reverse Words in a String III (Easy)/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public string ReverseWords(string s)
+    {
+        StringBuilder result = new();
+        int wordStartIndex = 0, wordLength = 0;
+        foreach (char c in s)
+        {
+            if (c == ' ')
+            {
+                int i = wordStartIndex + wordLength - 1;
+                while (i >= wordStartIndex) result.Append(s[i--]);
+                result.Append(' ');
+                wordStartIndex += wordLength + 1;
+                wordLength = 0;
+            }
+            else wordLength++;
+        }
+
+        int j = wordStartIndex + wordLength - 1;
+        while (j >= wordStartIndex) result.Append(s[j--]);
+
+        return result.ToString();
+    }
+}
