6753c7e47853ed72a5b2031c31c0df0425073f33
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-02-28 13:29:22 +0200

    Solved 19

---
+++ 0019. Remove Nth Node From End of List (Med)/Program.cs
@@ -0,0 +1,53 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution s = new();
+        ListNode head = new(1, new(2, new(3, new(4, new(5)))));
+        head = s.RemoveNthFromEnd(head, 1);
+        while (head is not null)
+        {
+            Console.Write(head.val + " ");
+            head = head.next;
+        }
+        Console.WriteLine();
+        
+        head = new(1, new(2, new(3, new(4, new(5)))));
+        head = s.RemoveNthFromEnd(head, 3);
+        while (head is not null)
+        {
+            Console.Write(head.val + " ");
+            head = head.next;
+        }
+        Console.WriteLine();
+
+        head = new(1, new(2, new(3, new(4, new(5)))));
+        head = s.RemoveNthFromEnd(head, 5);
+        while (head is not null)
+        {
+            Console.Write(head.val + " ");
+            head = head.next;
+        }
+        Console.WriteLine();
+
+        head = new(1);
+        head = s.RemoveNthFromEnd(head, 1);
+        while (head is not null)
+        {
+            Console.Write(head.val + " ");
+            head = head.next;
+        }
+        Console.WriteLine();
+        
+        head = new(1, new(2));
+        head = s.RemoveNthFromEnd(head, 1);
+        while (head is not null)
+        {
+            Console.Write(head.val + " ");
+            head = head.next;
+        }
+        Console.WriteLine();
+
+
+    }
+}
---
+++ 0019. Remove Nth Node From End of List (Med)/Solution.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        ListNode slow = head, fast = head;
+        while (n-- > 0) fast = fast.next;
+        while (fast is not null && fast.next is not null)
+        {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        if (fast is null) return head.next;
+        slow.next = slow.next.next;
+        return head;
+    }
+}
