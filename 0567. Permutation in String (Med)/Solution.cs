89f540810f2c37c498b24bb553f50dcf50bb3b3b
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-03-01 20:56:53 +0200

    Solved 567

---
+++ 0567. Permutation in String (Med)/Solution.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public bool CheckInclusion(string s1, string s2)
+    {
+        s1 = SortString(s1);
+        for (int i = 0; i < s2.Length - s1.Length + 1; i++)
+            if (s1 == SortString(s2.Substring(i, s1.Length))) return true;
+        return false;
+    }
+
+    private string SortString(string s)
+    {
+        var chars = s.ToCharArray();
+        Array.Sort(chars);
+        return new string(chars);
+    }
+}
