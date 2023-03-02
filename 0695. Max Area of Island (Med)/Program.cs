f2033dc4cc55ac505d81389da0f0292f35c1a869
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-03-02 13:56:43 +0200

    Solved 695

---
+++ 0695. Max Area of Island (Med)/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution s = new();
+        int[][] grid = new int[8][]
+        {
+            new int[] { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+            new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+            new int[] { 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+            new int[] { 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0 },
+            new int[] { 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0 },
+            new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+            new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+            new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 }
+        };
+        Console.WriteLine(s.MaxAreaOfIsland(grid));
+    }
+}
---
+++ 0695. Max Area of Island (Med)/Solution.cs
@@ -0,0 +1,48 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
+
+public class Solution
+{
+    public int MaxAreaOfIsland(int[][] grid)
+    {
+        int maxArea = 0;
+        var nextLand = GetNextLand(grid);
+        while (nextLand != (-1, -1))
+        {
+            maxArea = Math.Max(maxArea, GetLandArea(ref grid, nextLand.Item1, nextLand.Item2));
+            nextLand = GetNextLand(grid);
+        }
+        return maxArea;
+    }
+
+    private (int, int) GetNextLand(int[][] grid)
+    {
+        for (int i = 0; i < grid.Length; i++)
+            for (int j = 0; j < grid[0].Length; j++)
+                if (grid[i][j] == 1) return (i, j);
+        return (-1, -1);
+    }
+
+    private int GetLandArea(ref int[][] grid, int r, int c)
+    {
+        int area = 0;
+        Queue<(int, int)> queue = new();
+        queue.Enqueue((r, c));
+        while (queue.Count > 0)
+        {
+            var cell = queue.Dequeue();
+            if (grid[cell.Item1][cell.Item2] == -1) continue;
+            if (cell.Item1 + 1 < grid.Length && grid[cell.Item1 + 1][cell.Item2] == 1)
+                queue.Enqueue((cell.Item1 + 1, cell.Item2));
+            if (cell.Item1 - 1 >= 0 && grid[cell.Item1 - 1][cell.Item2] == 1)
+                queue.Enqueue((cell.Item1 - 1, cell.Item2));
+            if (cell.Item2 + 1 < grid[0].Length && grid[cell.Item1][cell.Item2 + 1] == 1)
+                queue.Enqueue((cell.Item1, cell.Item2 + 1));
+            if (cell.Item2 - 1 >= 0 && grid[cell.Item1][cell.Item2 - 1] == 1)
+                queue.Enqueue((cell.Item1, cell.Item2 - 1));
+            grid[cell.Item1][cell.Item2] = -1;
+            area++;
+        }
+        return area;
+    }
+}
