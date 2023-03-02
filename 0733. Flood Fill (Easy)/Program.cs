11f1ceb28ab8baba9383ec2c4676f511e2cce0c1
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-03-02 13:34:49 +0200

    Solved 733

---
+++ 0733. Flood Fill (Easy)/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution s = new();
+        int[][] img = new int[3][]
+        {
+            new int[] { 1, 1, 1 },
+            new int[] { 1, 1, 0 },
+            new int[] { 1, 0, 1 }
+        };
+        img = s.FloodFill(img, 1, 1, 2);
+        Console.WriteLine(string.Join('\n', img.Select(x => string.Join(',', x))));
+
+        img = new int[2][]
+        {
+            new int[] { 0, 0, 0 },
+            new int[] { 0, 0, 0 }
+        };
+        img = s.FloodFill(img, 0, 0, 0);
+        Console.WriteLine(string.Join('\n', img.Select(x => string.Join(',', x))));
+    }
+}
---
+++ 0733. Flood Fill (Easy)/Solution.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int[][] FloodFill(int[][] image, int sr, int sc, int color)
+    {
+        int oldColor = image[sr][sc];
+        if (oldColor == color) return image;
+        Queue<(int, int)> queue = new();
+        queue.Enqueue((sr, sc));
+        while (queue.Count > 0)
+        {
+            var cell = queue.Dequeue();
+            if (cell.Item1 + 1 < image.Length && image[cell.Item1 + 1][cell.Item2] == oldColor)
+                queue.Enqueue((cell.Item1 + 1, cell.Item2));
+            if (cell.Item1 - 1 >= 0 && image[cell.Item1 - 1][cell.Item2] == oldColor)
+                queue.Enqueue((cell.Item1 - 1, cell.Item2));
+            if (cell.Item2 + 1 < image[0].Length && image[cell.Item1][cell.Item2 + 1] == oldColor)
+                queue.Enqueue((cell.Item1, cell.Item2 + 1));
+            if (cell.Item2 - 1 >= 0 && image[cell.Item1][cell.Item2 - 1] == oldColor)
+                queue.Enqueue((cell.Item1, cell.Item2 - 1));
+            image[cell.Item1][cell.Item2] = color;
+        }
+        return image;
+    }
+}
