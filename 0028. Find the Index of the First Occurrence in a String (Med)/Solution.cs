a37078c4c6af75e3cb076361e81cc35c8d805a88
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-03-03 13:56:32 +0200

    Solved 28

---
+++ 0028. Find the Index of the First Occurrence in a String (Med)/Solution.cs
@@ -0,0 +1,13 @@
+﻿public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        for (int i = 0; i < haystack.Length - needle.Length + 1; i++)
+        {
+            int j = 0;
+            while (j < needle.Length && haystack[i + j] == needle[j]) j++;
+            if (j == needle.Length - 1) return i;
+        }
+        return -1;
+    }
+}
