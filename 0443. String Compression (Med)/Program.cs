93203938ab9e74fbfa99a288ceafee98ad40a186
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-03-03 13:09:58 +0200

    Solved 443

---
+++ 0443. String Compression (Med)/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution s = new();
+        var chars = "aaabbaa".ToCharArray();
+        Console.WriteLine(s.Compress(chars));
+        Console.WriteLine(string.Join("", chars));
+
+        chars = "abc".ToCharArray();
+        Console.WriteLine(s.Compress(chars));
+        Console.WriteLine(string.Join("", chars));
+    }
+}
---
+++ 0443. String Compression (Med)/Solution.cs
@@ -0,0 +1,40 @@
+﻿public class Solution
+{
+    public int Compress(char[] chars)
+    {
+        int startIndex = 0;
+        int nextIndex = 0;
+        for (int i = 0; i < chars.Length; i++)
+            if (chars[i] != chars[startIndex])
+                WriteChars(chars, ref startIndex, ref nextIndex, i);
+        if (startIndex < chars.Length - 1)
+            WriteChars(chars, ref startIndex, ref nextIndex, chars.Length);
+        if (startIndex == chars.Length - 1)
+            WriteChars(chars, ref startIndex, ref nextIndex, chars.Length);
+        return nextIndex;
+    }
+
+    private void WriteChars(char[] chars, ref int startIndex, ref int nextIndex, int i)
+    {
+        int count = i - startIndex;
+        if (count > 1)
+        {
+            string s = NumToChars(count);
+            chars[nextIndex++] = chars[startIndex];
+            foreach (char c in s) chars[nextIndex++] = c;
+            startIndex = i;
+        }
+        else chars[nextIndex++] = chars[startIndex++];
+    }
+
+    private string NumToChars(int n)
+    {
+        string s = "";
+        while(n > 0)
+        {
+            s = n % 10 + s;
+            n /= 10;
+        }
+        return s;
+    }
+}
