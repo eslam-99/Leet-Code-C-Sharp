3c06ed2e471a6d1ce7ed7ade40d9352b7f1148e4
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-03-03 13:39:20 +0200

    Solved 116

---
+++ 0116. Populating Next Right Pointers in Each Node (Med)/Solution.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public Node Connect(Node root)
+    {
+        int level = 0;
+        Stack<(Node, int)> s = new();
+        List<Node> list = new();
+        Node current = root;
+        while (current is not null || s.Count > 0)
+        {
+            while (current is not null)
+            {
+                s.Push((current, level));
+                if (list.Count > level)
+                {
+                    list[level].next = current;
+                    list[level] = current;
+                }
+                else list.Add(current);
+                level++;
+                current = current.left;
+            }
+
+            (current, level) = s.Pop();
+            current = current.right;
+            level++;
+        }
+        return root;
+    }
+}
