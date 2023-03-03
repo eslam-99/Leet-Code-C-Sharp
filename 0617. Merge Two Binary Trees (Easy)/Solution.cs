0688a9b40938cbc474ab86d5d37f19e89e7af1b2
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-03-03 13:14:38 +0200

    Solved 617

---
+++ 0617. Merge Two Binary Trees (Easy)/Solution.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+    {
+        if (root1 is null && root2 is null) return null!;
+        else if (root1 is null) return root2;
+        else if (root2 is null) return root1;
+
+        root1.val += root2.val;
+        root1.left = MergeTrees(root1.left, root2.left);
+        root1.right = MergeTrees(root1.right, root2.right);
+        return root1;
+    }
+}
