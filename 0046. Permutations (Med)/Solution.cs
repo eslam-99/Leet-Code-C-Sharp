af006b251839a24d022a528cc71242993f4eab56
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-03-05 19:16:22 +0200

    Solved 46

---
+++ 0046. Permutations (Med)/Solution.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    private List<IList<int>> result = new();
+    public IList<IList<int>> Permute(int[] nums)
+    {
+        Backtrack(nums, 0);
+        return result;
+    }
+
+    private void Backtrack(int[] nums, int start)
+    {
+        if (start == nums.Length)
+        {
+            result.Add(nums.ToList());
+            return;
+        }
+        for (int i = start; i < nums.Length; i++)
+        {
+            (nums[start], nums[i]) = (nums[i], nums[start]);
+            Backtrack(nums, start + 1);
+            (nums[start], nums[i]) = (nums[i], nums[start]);
+        }
+    }
+}
