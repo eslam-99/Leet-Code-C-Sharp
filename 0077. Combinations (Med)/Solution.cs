6430667275b53a03a26fd2b84cfb52d9e74f2616
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-03-05 19:57:15 +0200

    Solved 77

---
+++ 0077. Combinations (Med)/Solution.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    private List<IList<int>> result = new();
+    public IList<IList<int>> Combine(int n, int k)
+    {
+        Backtrack(new(), 1, n, k);
+        return result;
+    }
+    private void Backtrack(List<int> nums, int next, int n, int k)
+    {
+        if (nums.Count == k)
+        {
+            result.Add(nums.ToList());
+            return;
+        }
+        for (int i = next; i <= n; i++)
+        {
+            nums.Add(i);
+            Backtrack(nums, i + 1, n, k);
+            nums.RemoveAt(nums.Count - 1);
+        }
+    }
+}
