dd70897e89321f3927d28e1436c1282d746a379a
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-03-06 14:41:04 +0200

    Solved 784

---
+++ 0784. Letter Case Permutation (Med)/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution s = new();
+        Console.WriteLine(string.Join('\n', s.LetterCasePermutation("ab")));
+        Console.WriteLine();
+        Console.WriteLine(string.Join('\n', s.LetterCasePermutation("a1b2")));
+    }
+}
---
+++ 0784. Letter Case Permutation (Med)/Solution.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    List<string> result = new();
+    public IList<string> LetterCasePermutation(string s)
+    {
+        result = new();
+        Backtrack(s, 0);
+        return result;
+    }
+
+    private void Backtrack(string s, int index)
+    {
+        if (index == s.Length) result.Add(s);
+        else if (char.IsLetter(s[index]))
+        {
+            string s1 = s[..index] + char.ToLower(s[index]) + s[(index + 1)..];
+            Backtrack(s1, index + 1);
+            string s2 = s[..index] + char.ToUpper(s[index]) + s[(index + 1)..];
+            Backtrack(s2, index + 1);
+        }
+        else Backtrack(s, index + 1);
+    }
+}
