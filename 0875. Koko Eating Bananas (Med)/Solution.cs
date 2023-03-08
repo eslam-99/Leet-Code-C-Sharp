2f2269c53789a969409f23d250d284782fbb16c9
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-03-08 17:12:46 +0200

    Solved  875

---
+++ 0875. Koko Eating Bananas (Med)/Solution.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int MinEatingSpeed(int[] piles, int h)
+    {
+        if (piles.Length == h) return piles.Max();
+        int left = 1, right = piles.Max();
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            int c = 0;
+            foreach (int p in piles)
+                c += Math.Max((int)Math.Ceiling((decimal)p / mid), 1);
+            if (c <= h) right = mid;
+            else left = mid + 1;
+        }
+        return left;
+    }
+}
