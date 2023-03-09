360a8050886b9ae7c8beeb194a8a0f030205645e
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-03-09 13:31:58 +0200

    Solved 142

---
+++ 0142. Linked List Cycle II (Med)/Solution.cs
@@ -0,0 +1,11 @@
+﻿public class Solution
+{
+    public ListNode DetectCycle(ListNode head)
+    {
+        HashSet<ListNode> nodes = new();
+        while (head is not null)
+            if (!nodes.Add(head)) return head;
+            else head = head.next;
+        return head!;
+    }
+}
