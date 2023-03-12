2992479daa6384cb667ac3948095fa78f8b8f1a5
Eslam Mohamed <eslam.mohamed.551999@gmail.com>
2023-03-12 17:59:14 +0200

    Solved 23

---
+++ 0023. Merge k Sorted Lists (Hard)/Solution.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public ListNode MergeKLists(ListNode[] lists)
+    {
+        if (lists.Length == 0) return null!;
+        ListNode head = new(), cur = head;
+        HashSet<int> activeNodesIndexes = new();
+        for (int i = 0; i < lists.Length; i++)
+            if (lists[i] is not null) activeNodesIndexes.Add(i);
+        while (activeNodesIndexes.Count > 0)
+        {
+            int minValIndex = activeNodesIndexes.First();
+            foreach (int index in activeNodesIndexes)
+                if (lists[index].val < lists[minValIndex].val) minValIndex = index;
+            cur.next = lists[minValIndex];
+            cur = cur.next;
+            lists[minValIndex] = lists[minValIndex].next;
+            if (lists[minValIndex] is null) activeNodesIndexes.Remove(minValIndex);
+        }
+        return head.next;
+    }
+}
